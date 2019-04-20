36f830d6fa4445f14c3a06afc48b981098dddff3
fyodorussia <fvdelows@mail.ru>
2019-04-20 14:50:58 +0300

    убрал loging(в комент)

--- Alisa/flask_app.py
+++ Alisa/flask_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import logging
+#import logging
 from alice_sdk import *
 from main_function import *
 
@@ -15,12 +15,12 @@ def hello_world():
 def main():
     alice_request = AliceRequest(request.json)
     alice_response = AliceResponse(alice_request)
-    logging.info("Request {}".format(alice_request))
+    #logging.info("Request {}".format(alice_request))
     session_storage = {}
     alice_response, session_storage[alice_request.user_id] = handle_diolog(
         alice_request, alice_response, session_storage.get(alice_request.user_id)
     )
-    logging.info(f"Response {alice_response}")
+    #logging.info(f"Response {alice_response}")
     return alice_response.dumps()
 
 
