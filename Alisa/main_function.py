1daf52e513ef9b7b437744a7577a74683c06d0f4
fyodorussia <fvdelows@mail.ru>
2019-04-20 14:42:11 +0300

    правка 1

--- Alisa/main_function.py
+++ Alisa/main_function.py
@@ -1,5 +1,5 @@
 from little_functions import *
 
-def handle_diolog(request, response, user_storage):
+def handle_diolog(request, response, user_storage={}):
     output_message = "Hello, word."
     return message_return(response, user_storage, output_message)
