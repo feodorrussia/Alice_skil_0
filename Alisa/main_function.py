ac84483011dfd3a036362ee73bfb59a81d9275cc
fyodorussia <fvdelows@mail.ru>
2019-04-20 13:57:24 +0300

    начальная оболочка навыка

---
+++ Alisa/main_function.py
@@ -0,0 +1,5 @@
+from little_functions import *
+
+def handle_diolog(request, response, user_storage):
+    output_message = "Hello, word."
+    return message_return(response, user_storage, output_message)
